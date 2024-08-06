8cb4f6e495cace811f0df3dcb343bdbd6634e029
leah5520 <aalirod9625@gmail.com>
2024-08-06 16:15:09 +0000

    added age verify

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log('Hello World!')
+// console.log('Hello World!');
+
+// var prompt=prompt('what is your name');
+
+// console.log(prompt);
+
+var age = prompt('whats your age?');
+console.log(age)
+
+
+function verifyAge(age) {
+  if (age < 18) {
+    alert('your are a minor')
+  } else if (age >=18 && age <= 60) { alert ('you are an adult!')
+  } else { alert('you are a senior citizen')
+  }
+}
